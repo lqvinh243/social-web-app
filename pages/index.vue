157868b96f178b3b80212c9a0af78b024baf27c9
thanhla <staan0906@gmail.com>
2021-11-13 23:58:49 +0700

    Revert "update code new user in main"
    This reverts commit e024cd564dae79ec86363d51b94a0ad97a0c4f69.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class=" container row">
-        <div class="col-md-10">
+    <div class="home row mx-0">
+        <div class="col-md-8">
             <div class="status my-3 d-flex">
                 <img
                     :src="getAvatar"
@@ -18,57 +18,10 @@
                 <el-empty :image-size="200" description="No post available" />
             </div>
         </div>
-        <div class="col-md-2">
-            <div
-                class="suggestion"
-                style="margin-top: 130px;position: fixed;"
-            >
-                <div class="mt-4">
-                    <div class="d-flex justify-content-between align-items-center my-2">
-                        <h5 class="text-danger">
-                            New User
-                        </h5>
-                    </div>
-                    <div class="suggestions">
-                        <div
-                            v-for="(item,index) in newUser"
-                            :key="index"
-                            class="
-                      d-flex
-                      p-2
-                      align-items-center
-                      justify-content-between
-                      w-100
-                    "
-                            style="border-bottom: 1px solid #3333;"
-                        >
-                            <div>
-                                <div class="d-flex align-items-center">
-                                    <img
-                                        :src="item.avatar"
-                                        class="big-avatar"
-                                    >
-                                    <div class="ml-3 mr-3" style="transform: translateY(-2px)">
-                                        <nuxt-link :to="`/profile/${item._id}`">
-                                            <span class="d-block">{{ item.fullname }}</span>
-                                        </nuxt-link>
-                                        <small style="opacity: 0.7">
-                                            <div style="filter: invert(0)">
-                                                Suggested for you
-                                            </div>
-                                            <div />
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 <script lang="ts">
+
 import Vue from 'vue';
 import { mapGetters } from 'vuex';
 import CreatePostModal from '~/components/modals/CreatePostModal.vue';
@@ -76,6 +29,7 @@ import PostItem from '~/components/PostItem.vue';
 import eventBus from '~/plugins/event-bus';
 import { postsService } from '~/services/posts';
 import { connectWS } from '~/utils/socket';
+
 export default Vue.extend({
     components: { PostItem, CreatePostModal },
     layout: 'default',
@@ -83,8 +37,8 @@ export default Vue.extend({
     data: () => ({
         postList: [],
         content: '',
-        dialogVisiblePost: false,
-        newUser: [],
+        dialogVisiblePost: false
+
     }),
     computed: {
         ...mapGetters('auth', ['profile']),
@@ -99,49 +53,53 @@ export default Vue.extend({
         this.$nextTick(() => {
             this.init();
         });
+
         const socket = connectWS(this.$config.wsUrl, 'like', this.$store.state.auth.accessToken);
         socket.on('connect', () => {
             console.log('Like channel is connected!');
         });
+
         socket.on('disconnect', () => {
             // eslint-disable-next-line no-console
             console.log('socket is disconnected!');
         });
+
         socket.on('like_post', (data) => {
             eventBus.$emit('LIKE_POST', data);
         });
+
         socket.on('unlike_post', (data) => {
             eventBus.$emit('UNLIKE_POST', data);
         });
+
         const socketComment = connectWS(this.$config.wsUrl, 'comment', this.$store.state.auth.accessToken);
         socketComment.on('connect', () => {
             console.log('Comment channel is connected!');
         });
+
         socketComment.on('disconnect', () => {
             // eslint-disable-next-line no-console
             console.log('socketComment is disconnected!');
         });
+
         socketComment.on('comment_post', (data) => {
             eventBus.$emit('COMMENT_POST', data);
         });
     },
+
     destroyed() {
         eventBus.$destroy();
     },
+
     methods: {
         init() {
             this.$nuxt.$loading.start();
-            Promise.all([
-                postsService.getPosts(),
-                this.$axios.$get('/api/v1/suggestionsUser')
-            ])
+            postsService.getPosts()
                 .then(res => {
-                    this.postList = res[0].posts;
-                    this.newUser = res[1].users.slice(0, 5);
+                    this.postList = res.posts;
                 })
                 .catch(() => {
                     this.postList = [];
-                    this.newUser = [];
                 })
                 .finally(() => {
                     this.$nuxt.$loading.finish();
@@ -152,7 +110,7 @@ export default Vue.extend({
         },
         closeDialogPost() {
             this.dialogVisiblePost = false;
-        },
+        }
     }
 });
 </script>
